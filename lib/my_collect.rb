2330594762fc4b4b344187f34fed351da3023454
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-16 05:26:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,16 @@
 def my_collect(array)
   
   i = 0
+  collection = []
   
   while i < array.length
   
-    yield array[i]
+    collection << yield array[i]
     i += 1
     
   end
   
+  collection
   
 end
 
